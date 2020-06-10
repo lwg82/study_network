8d6005ee562e3d9f5b0dce13f41b064268303edf
lwg82 <shuangyan5230@163.com>
2020-06-10 05:04:55 -0400

    add tcp server

---
+++ ch1_daytime_tcp_client_IPv6/daytime_tcp_client.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/errno.h>
+
+#include <sys/socket.h> // socket
+#include <netinet/in.h> // socketaddr_in
+#include <arpa/inet.h>	// inet_pton
+#include <unistd.h>     // read
+
+#include "../include/easy_log.h"
+
+
+
+int main(int argc, char *argv[])
+{
+  char *ip="172.18.12.68";
+  
+  int nSockFD;
+  
+  struct sockaddr_in6   server_address;
+  
+  int read_bytes = 0;
+  char recv_buf[BUF_STRING_LEN_4K];
+  int error_code;
+  
+  // 创建 socket
+  if((nSockFD = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+      CONSOLE_ERROR_LOG(nSockFD);
+  }
+  // 服务器地址
+  bzero(&server_address, sizeof(server_address));
+  server_address.sin6_family = AF_INET;
+  server_address.sin6_port = htons(13);
+  
+  if(inet_pton(AF_INET6, ip, &server_address.sin6_addr) <= 0)
+    CONSOLE_ERROR_LOG_FMT(0, "inet_pton error for %s", ip);
+  
+  if((error_code = connect(nSockFD, (struct sockaddr*)&server_address, sizeof(server_address))) < 0)
+    CONSOLE_ERROR_LOG(error_code);
+    
+  while((read_bytes = read(nSockFD, recv_buf, BUF_STRING_LEN_4K)) > 0)
+  {
+     recv_buf[read_bytes] = 0;
+     
+     if(fputs(recv_buf, stdout) == EOF)
+      CONSOLE_ERROR_LOG_FMT(0, "fputs error");
+  }
+  
+   if(read_bytes < 0)
+        CONSOLE_ERROR_LOG_FMT(read_bytes, "read error");
+        
+  
+  exit(EXIT_SUCCESS);
+}
--- ch1_daytime_tcp_server/daytime_tcp_server.c
+++ ch1_daytime_tcp_server/daytime_tcp_server.c
@@ -3,10 +3,68 @@
 
 #include <sys/errno.h>
 
+#include <sys/socket.h> // socket
+#include <netinet/in.h> // socketaddr_in
+#include <arpa/inet.h>	// inet_pton
+#include <unistd.h>     // read
+#include <time.h>       // time_t
 
+#include "../include/easy_log.h"
+
+/*
+ * netstat -ntlp | grep 13 # 查看 TCP 13 端口监听情况
+ */
 int main(int argc, char *argv[])
 {
-  int nListenFD;
+  time_t ticks;
+  char buf[BUF_STRING_LEN_4K];
+
+  int socket_listen_fd;
+  int socket_conn_fd;
+  
+  struct sockaddr_in server_addr;
+  
+  char *ptr;
+  int backlog = SOMAXCONN;
+  
+  socket_listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+  
+  bzero(&server_addr, sizeof(server_addr));
+  
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(13);
+  
+  // bind
+  if(bind(socket_listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+     CONSOLE_ERROR_LOG_FMT(0, "bind error");
+     
+  // listen
+  if((ptr = getenv("LISTENQ")) != NULL)
+    backlog = atoi(ptr);
+    
+  if(listen(socket_listen_fd, backlog) < 0)
+     CONSOLE_ERROR_LOG_FMT(0, "listen error");
+  
+  for(;;)
+  {
+    socket_conn_fd = accept(socket_listen_fd, (struct sockaddr*)NULL, NULL);
+    if(socket_conn_fd < 0)
+      CONSOLE_ERROR_LOG_FMT(0, "accept error");
+      
+    printf("client connect fd=%d\r\n", socket_conn_fd);
+      
+    ticks = time(NULL);
+    bzero(buf, BUF_STRING_LEN_4K);
+    
+    int nBytes = snprintf(buf, BUF_STRING_LEN_4K, "%.24s\r\n", ctime(&ticks));
+    
+    if(write(socket_conn_fd, buf, nBytes) < 0)
+      CONSOLE_ERROR_LOG_FMT(0, "write error");
+      
+    if(close(socket_conn_fd) == -1)
+      CONSOLE_ERROR_LOG_FMT(0, "close error");
+  }
   
   exit(EXIT_SUCCESS);
 }
